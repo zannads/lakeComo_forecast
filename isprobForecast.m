4d492bb8f2a5f52bc93bfac89b5ee3e52fca6518
Dennis <dennis.zanutto@mail.polimi.it>
2021-12-23 15:06:48 +0100

    class moved to parser

--- isprobForecast.m
+++
@@ -1,6 +0,0 @@
-function outputArg = isprobForecast(inputArg1)
-%ISPROBFORECAST returns true if the input is a probForecast class.
-
-    outputArg = strcmp( class( inputArg1), probForecast.type );
-end
-
