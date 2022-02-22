845b6bf3152b41e7720ba80e179311ebd181f8fe
Dennis <dennis.zanutto@mail.polimi.it>
2022-02-22 17:20:32 +0100

    speed constraint check

--- draws/draw_ddp_traj.m
+++ draws/draw_ddp_traj.m
@@ -46,8 +46,13 @@ p.CoordinateTickLabels = ["FloodDays [day/year]", "Deficit [??]", "Static low [d
 title( combination_title );
 %%
 %weight_selector( JJJ, [4.45; 689.194; 10.85] )
+speed_constraint_check( sim_r, sim_h)
 
 function o = weight_selector( J, value )
     o = sum( (J-value).^2, 1);
     [~, o] = min( o );
+end
+
+function outp = speed_constraint_check( r, h )
+    outp = sum( ( (r(2:end, :)-r(1:end-1,:))>240 & h(1:end-1,:)<0.8 ) | ( (r(2:end,:)-r(1:end-1,:))>360 & h(1:end-1,:)>=0.8 & h(1:end-1,:)<=1.1), 1 );
 end
